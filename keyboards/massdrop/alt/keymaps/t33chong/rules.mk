fbfd6604b114db2fea041ecf925083948b356067
Tristan Chong <tristanchong@gmail.com>
2021-03-07 04:20:00 -0800

    Base keymap on default_md, enable mousekey, set default RGB

---
+++ keyboards/massdrop/alt/keymaps/t33chong/rules.mk
@@ -0,0 +1,4 @@
+# This keymap requires Massdrop Configurator support
+OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
+BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
