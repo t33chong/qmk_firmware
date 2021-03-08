09f93673e43783857bf5d45fd5ca81ab26392519
Tristan Chong <tristanchong@gmail.com>
2021-03-07 20:03:13 -0800

    Indicate layer with rgb_matrix_mode

--- keyboards/massdrop/alt/keymaps/t33chong/keymap.c
+++ keyboards/massdrop/alt/keymaps/t33chong/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
-    [1] = LAYOUT_65_ansi_blocker(
+    [1] = LAYOUT_65_ansi_blocker( // Mouse keys
      /* KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \ */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
      /* KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,  \ */
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \ */
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
-    [15] = LAYOUT_65_ansi_blocker( // ALT swaps ALT with CMD, CMD resets it
+    [15] = LAYOUT_65_ansi_blocker( // Hyper; ALT swaps ALT with CMD, CMD resets it
      /* KC_GRV,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,        KC_7,      KC_8,         KC_9,        KC_0,         KC_MINS,      KC_EQL,       KC_BSPC,      KC_HOME, \ */
         HYPR(KC_GRV),KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,       KC_F7,     KC_F8,        KC_F9,       KC_F10,       KC_F11,       KC_F12,       HYPR(KC_BSPC),MD_BOOT, \
      /* KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,        KC_U,      KC_I,         KC_O,        KC_P,         KC_LBRC,      KC_RBRC,      KC_BSLS,      KC_END,  \ */
@@ -52,6 +52,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     */
 };
+
+// Indicate layer with RGB matrix effect
+// https://docs.qmk.fm/#/custom_quantum_functions?id=layer-change-code
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case 1: // Mouse keys
+        rgb_matrix_mode(RGB_MATRIX_GRADIENT_UP_DOWN);
+        break;
+    case 15: // Hyper
+        rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+        break;
+    default:
+        rgb_matrix_mode(RGB_MATRIX_GRADIENT_LEFT_RIGHT);
+        break;
+    }
+  return state;
+}
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
