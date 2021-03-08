5507de01643bd4d334b1b387849ac7b73b9ec236
Tristan Chong <tristanchong@gmail.com>
2021-03-07 16:47:48 -0800

    Base keymap on default instead of default_md

--- keyboards/massdrop/alt/keymaps/t33chong/rules.mk
+++ keyboards/massdrop/alt/keymaps/t33chong/rules.mk
@@ -1,4 +1,2 @@
-# This keymap requires Massdrop Configurator support
-OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
 BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
