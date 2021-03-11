9987b237b710da3edc47d4c11e95c36b7c4b3107
Tristan Chong <tristanchong@gmail.com>
2021-03-11 13:10:38 -0800

    Restore lctrl+lshift = numlock (caps)

--- keyboards/massdrop/alt/keymaps/t33chong/keymap.c
+++ keyboards/massdrop/alt/keymaps/t33chong/keymap.c
@@ -16,9 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,  \
         LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         LSFT_T(KC_BSPC),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_EQL),   KC_UP,   KC_PGDN, \
-        /* KC_NLCK,        KC_LALT, KC_LGUI,                            HYPR_T(KC_SPC),                     KC_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \ */
         MO(2),          KC_LALT, KC_LGUI,                            HYPR_T(KC_SPC),                     KC_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \
-        /* KC_LCTL,        KC_LALT, KC_LGUI,                            HYPR_T(KC_SPC),                     KC_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \ */
     ),
     [1] = LAYOUT_65_ansi_blocker( // Mouse keys
      /* KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \ */
@@ -40,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \ */
         _______, _______, _______, _______, _______, _______, KC_BSPC, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, _______,          _______, _______, \
      /* KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \ */
-        _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PAST, _______,          _______, _______, \
+        KC_NLCK, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PAST, _______,          _______, _______, \
      /* KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_UNDS, MO,      KC_LEFT, KC_DOWN, KC_RGHT  \ */
         _______, _______, _______,                            KC_KP_0,                            _______, _______, _______, _______, _______  \
     ),
