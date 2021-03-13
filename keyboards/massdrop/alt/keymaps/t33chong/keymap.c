f5c68fc46f3be37e45c89c4ad0db56e081bb1403
Tristan Chong <tristanchong@gmail.com>
2021-03-13 14:18:56 -0800

    Add gaming layers

--- keyboards/massdrop/alt/keymaps/t33chong/keymap.c
+++ keyboards/massdrop/alt/keymaps/t33chong/keymap.c
@@ -11,21 +11,35 @@ enum alt_keycodes {
     NEW_SAFE_RANGE
 };
 
-#define MY_CTES LCTL_T(KC_ESC)
-#define MY_SFBS LSFT_T(KC_BSPC)
-#define MY_HYSP HYPR_T(KC_SPC)
-#define MY_SFEQ RSFT_T(KC_EQL)
-
 enum my_keycodes {
     MY_UNDS = NEW_SAFE_RANGE, //Use instead of KC_UNDS to avoid shift applying to next keypress
+    /* MY_ALBS, */
+    /* MY_TGGA //Toggle gaming layer */
 };
+
+/* // Tap Dance definitions */
+/* qk_tap_dance_action_t tap_dance_actions[] = { */
+/*     // Tap once for Escape, twice for Caps Lock */
+/*     [MY_TGGA] = ACTION_TAP_DANCE_DOUBLE(TG(3), TG(0)), */
+/* }; */
+
+#define MY_CTES LCTL_T(KC_ESC)
+#define MY_SFBS LSFT_T(KC_BSPC)
+/* #define MY_SFBS LSFT_T(LALT(KC_BSPC)) */
+/* #define MY_ALBS LALT(KC_BSPC) */
+/* #define MY_ALBS (QK_LALT | KC_BSPC) */
+/* #define MY_SFBS LSFT_T(MY_ALBS) */
+#define MY_HYSP HYPR_T(KC_SPC)
+#define MY_SFEQ RSFT_T(KC_EQL)
+#define MY_ESLT LT(4, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,  \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TG(3),   \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  \
         MY_CTES, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         MY_SFBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_SFEQ,          KC_UP,   KC_PGDN, \
+        /* KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_SFEQ,          KC_UP,   KC_PGDN, \ */
         MO(2),   KC_LALT, KC_LGUI,                            MY_HYSP,                            MY_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker( // Mouse keys
@@ -42,12 +56,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NLCK, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PAST, _______,          _______, _______, \
         _______, _______, _______,                            KC_KP_0,                            _______, _______, _______, _______, _______  \
     ),
+    [3] = LAYOUT_65_ansi_blocker( // Gaming
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        MY_ESLT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        KC_LCTL, KC_EQL,  KC_LALT,                            _______,                            _______, KC_LGUI, _______, _______, _______  \
+    ),
+    [4] = LAYOUT_65_ansi_blocker( // Gaming arrows/mirror image
+        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, KC_P,    KC_UP,   KC_I,    KC_U,    KC_Y,    _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_J,    KC_H,    _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, KC_SLSH, KC_DOT,  KC_COMM, KC_M,    KC_N,    _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            KC_ENT,                             _______, _______, _______, _______, _______  \
+    ),
     [15] = LAYOUT_65_ansi_blocker( // Function; ALT swaps ALT with CMD, CMD resets it
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, MD_BOOT, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         TG(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, _______, \
-        _______, LAG_SWP, LAG_NRM,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
+        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
     /*
     [X] = LAYOUT_65_ansi_blocker(
@@ -64,6 +92,7 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MY_CTES:
         case MY_HYSP:
+        case MY_ESLT:
             return true;
         default:
             return false;
@@ -78,6 +107,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         rgb_matrix_mode(RGB_MATRIX_GRADIENT_UP_DOWN);
         break;
     case 2: // Numpad
+    case 3: // Gaming
+    case 4: // Gaming arrows/mirror image
     case 15: // Function
         rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
         break;
@@ -166,6 +197,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("_");
             }
             return false;
+        /* case MY_ALBS: */
+        /*     if (record->event.pressed) { */
+        /*         SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_BSPC) SS_UP(X_LALT)); */
+        /*         /1* SS_DOWN(KC_LALT); *1/ */
+        /*         /1* SS_TAP(KC_BSPC); *1/ */
+        /*         /1* SS_UP(KC_LALT); *1/ */
+        /*     } */
+        /*     /1* if (record->event.pressed) { *1/ */
+        /*     /1*     /2* register_code16(LALT(KC_BSPC)); *2/ *1/ */
+        /*     /1*     register_code16((QK_LALT | KC_BSPC)); *1/ */
+        /*     /1* } else { *1/ */
+        /*     /1*     /2* unregister_code16(LALT(KC_BSPC)); *2/ *1/ */
+        /*     /1*     unregister_code16((QK_LALT | KC_BSPC)); *1/ */
+        /*     /1* } *1/ */
+        /*     return false; */
         default:
             return true; //Process all other keycodes normally
     }
