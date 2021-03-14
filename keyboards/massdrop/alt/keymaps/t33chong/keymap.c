cd70ad5e18d4f8877e66d4e567657e28d9eb350b
Tristan Chong <tristanchong@gmail.com>
2021-03-14 04:56:36 -0700

    Change right shift tap from = to caps lock

--- keyboards/massdrop/alt/keymaps/t33chong/keymap.c
+++ keyboards/massdrop/alt/keymaps/t33chong/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+/* #include "colors.h" */
 
 enum alt_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
@@ -14,32 +15,26 @@ enum alt_keycodes {
 enum my_keycodes {
     MY_UNDS = NEW_SAFE_RANGE, //Use instead of KC_UNDS to avoid shift applying to next keypress
     /* MY_ALBS, */
-    /* MY_TGGA //Toggle gaming layer */
 };
-
-/* // Tap Dance definitions */
-/* qk_tap_dance_action_t tap_dance_actions[] = { */
-/*     // Tap once for Escape, twice for Caps Lock */
-/*     [MY_TGGA] = ACTION_TAP_DANCE_DOUBLE(TG(3), TG(0)), */
-/* }; */
 
 #define MY_CTES LCTL_T(KC_ESC)
 #define MY_SFBS LSFT_T(KC_BSPC)
+#define MY_HYSP HYPR_T(KC_SPC)
+#define MY_SFCA RSFT_T(KC_CAPS)
+#define MY_ESLT LT(4, KC_ESC)
 /* #define MY_SFBS LSFT_T(LALT(KC_BSPC)) */
 /* #define MY_ALBS LALT(KC_BSPC) */
 /* #define MY_ALBS (QK_LALT | KC_BSPC) */
 /* #define MY_SFBS LSFT_T(MY_ALBS) */
-#define MY_HYSP HYPR_T(KC_SPC)
-#define MY_SFEQ RSFT_T(KC_EQL)
-#define MY_ESLT LT(4, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TG(3),   \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  \
         MY_CTES, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-        MY_SFBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_SFEQ,          KC_UP,   KC_PGDN, \
-        /* KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_SFEQ,          KC_UP,   KC_PGDN, \ */
+        MY_SFBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_SFCA,          KC_UP,   KC_PGDN, \
+        /* KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MY_SFCA,          KC_UP,   KC_PGDN, \ */
+        /* MO(2),   KC_LALT, KC_LGUI,                            MY_HYSP,                            MY_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \ */
         MO(2),   KC_LALT, KC_LGUI,                            MY_HYSP,                            MY_UNDS, MO(15),  KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker( // Mouse keys
