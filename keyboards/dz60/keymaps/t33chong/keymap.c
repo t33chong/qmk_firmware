39f40c19b4d2068aa099162083a039b06f2a3c79
Tristan Chong <tristanchong@gmail.com>
2021-05-14 01:29:47 -0700

    Undo tap shift for caps

--- keyboards/dz60/keymaps/t33chong/keymap.c
+++ keyboards/dz60/keymaps/t33chong/keymap.c
@@ -37,7 +37,6 @@ enum my_keycodes {
 };
 
 #define _CTLESC CTL_T(KC_ESC)         // Hold for control, tap for escape
-#define _SFTCAP SFT_T(KC_CAPS)        // Hold for shift, tap for caps lock
 #define _MEHMIN LT(_MEH, KC_MINS)     // Hold for meh layer, tap for -
 #define _NUMSPC LT(_NUMERALS, KC_SPC) // Hold for numerals layer, tap for space
 #define _SFTEQL SFT_T(KC_EQL)         // Hold for shift, tap for =
@@ -49,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, \
     _CTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  \
     KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          _SFTEQL,          KC_UP,   \
-    _ARRMSK, KC_LALT, KC_LGUI,          _SFTCAP,          _MEHMIN,          _NUMSPC,          _UNDSCR, _HYPFUN, KC_LEFT, KC_RGHT, KC_DOWN  \
+    _ARRMSK, KC_LALT, KC_LGUI,          KC_LSFT,          _MEHMIN,          _NUMSPC,          _UNDSCR, _HYPFUN, KC_LEFT, KC_RGHT, KC_DOWN  \
   ),
   [_NUMERALS] = LAYOUT_t33chong(
     _______, G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5), G(KC_6), G(KC_7), G(KC_8), G(KC_9), G(KC_0), _______, _______, _______, _______, \
