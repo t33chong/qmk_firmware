611362bda0b49678b93aceb9d9e165c941e0bf16
Tristan Chong <tristanchong@gmail.com>
2021-05-15 03:23:07 -0700

    Make shift+backspace send alt+backspace

--- keyboards/dz60/keymaps/t33chong/keymap.c
+++ keyboards/dz60/keymaps/t33chong/keymap.c
@@ -14,9 +14,10 @@ enum my_keycodes {
   __RESET = SAFE_RANGE, // Restart into bootloader after hold timeout
   _ARRMSK,              // Hold to activate arrows layer, tap to toggle mousekeys layer
   _HYPFUN,              // Hold to activate hyper layer, tap to toggle function layer
-  _ALTBSP,              // Send alt+backspace
+  /* _ALTBSP,              // Send alt+backspace */
   _UNDSCR,              // Send underscore (used instead of KC_UNDS to avoid shift applying to next keypress)
   _MODGUI,              // Send command and set boolean flag
+  _MODSFT,              // Send shift and set boolean flag
 };
 
 #define H HYPR
@@ -33,13 +34,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, \
     _CTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  \
     KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_EQL,           KC_UP,   \
-    _ARRMSK, KC_LALT, _MODGUI,          KC_LSFT,          _NUMMIN,          _MEHSPC,          _UNDSCR, _HYPFUN, KC_LEFT, KC_RGHT, KC_DOWN  \
+    _ARRMSK, KC_LALT, _MODGUI,          _MODSFT,          _NUMMIN,          _MEHSPC,          _UNDSCR, _HYPFUN, KC_LEFT, KC_RGHT, KC_DOWN  \
   ),
   [_NUMERALS] = LAYOUT_t33chong(
     _______, G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5), G(KC_6), G(KC_7), G(KC_8), G(KC_9), G(KC_0), _______, _______, _______, _______, \
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, _______,          _______, \
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,                   _______, \
-    _ALTBSP, H(KC_1), H(KC_2), H(KC_3), H(KC_4), H(KC_5), H(KC_6), H(KC_7), H(KC_8), H(KC_9), H(KC_0),          _______,          _______, \
+    _______, H(KC_1), H(KC_2), H(KC_3), H(KC_4), H(KC_5), H(KC_6), H(KC_7), H(KC_8), H(KC_9), H(KC_0),          _______,          _______, \
     _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______  \
   ),
   [_ARROWS] = LAYOUT_t33chong(
@@ -169,14 +170,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint32_t _hypfun_hold_timer;
   static uint32_t _reset_hold_timer;
   static bool _is_gui_held;
+  static bool _is_shift_held;
   switch (keycode) {
-    case _ALTBSP:
-      if (record->event.pressed) {
-        register_code16(A(KC_BSPC));
-      } else {
-        unregister_code16(A(KC_BSPC));
-      }
-      return false;
+    /* case _ALTBSP: */
+    /*   if (record->event.pressed) { */
+    /*     register_code16(A(KC_BSPC)); */
+    /*   } else { */
+    /*     unregister_code16(A(KC_BSPC)); */
+    /*   } */
+    /*   return false; */
     case _ARRMSK:
       if (record->event.pressed) {
         _arrmsk_hold_timer = timer_read32();
@@ -195,6 +197,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         unregister_code(KC_LGUI);
         _is_gui_held = false;
+      }
+      return false;
+    case _MODSFT:
+      if (record->event.pressed) {
+        register_code(KC_LSFT);
+        _is_shift_held = true;
+      } else {
+        unregister_code(KC_LSFT);
+        _is_shift_held = false;
       }
       return false;
     case _HYPFUN:
@@ -232,7 +243,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           unregister_code(KC_LSFT);
         }
         return false;
-      } else if (_is_meh_active) { // Make backspace send forward delete when meh is held
+      } else if (_is_shift_held) { // Send alt+backspace when shift is held
+        if (record->event.pressed) {
+          unregister_code(KC_LSFT);
+          register_code16(A(KC_BSPC));
+        } else {
+          unregister_code16(A(KC_BSPC));
+          register_code(KC_LSFT);
+        }
+        return false;
+      } else if (_is_meh_active) { // Send forward delete when meh is held
         if (record->event.pressed) {
           register_code(KC_DEL);
         } else {
