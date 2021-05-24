3f42ed63734618e7d566d07528a4e0ca828547e9
Tristan Chong <tristanchong@gmail.com>
2021-05-24 13:49:56 -0700

    Restore previous tab key functionality and arrows layer, with C-z as push to talk

--- keyboards/dz60/keymaps/t33chong/keymap.c
+++ keyboards/dz60/keymaps/t33chong/keymap.c
@@ -18,7 +18,6 @@ enum my_keycodes {
 #define _CTLESC CTL_T(KC_ESC)          // Hold for control, tap for escape
 #define _MEHSPC LT(_MEH, KC_SPC)       // Hold for meh layer, tap for space
 #define _HYPEQL LT(_HYPER, KC_EQL)     // Hold for hyper layer, tap for =
-#define _TABARR LT(_ARROWS, KC_TAB)    // Hold for arrows layer, tap for tab
 #define _TTARRO TT(_ARROWS)            // Tap-toggle arrows layer
 #define _MOFUNC MO(_FUNCTION)          // Hold for function layer
 #define _MONUMS MO(_NUMERALS)          // Hold for numerals layer
@@ -39,7 +38,7 @@ enum my_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT_t33chong(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BRMD, KC_BRMU, KC_VOLD, KC_VOLU, \
-    _TABARR, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          _TGMOUS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          _TGMOUS, \
     _CTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  \
     KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          _HYPEQL,          _MOFUNC, \
     _TTARRO, KC_LALT, _MODGUI,          KC_LSFT,          _MONUMS,          _MEHSPC,          KC_MINS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
@@ -53,10 +52,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_ARROWS] = LAYOUT_t33chong(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, KC_LSFT, KC_LALT, KC_LGUI, _______, _______, KC_BSPC, KC_DEL,  _______, _______, _______, _______,          _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,                   _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______, \
-    _______, _PUSHTT, _______,          KC_BSPC,          KC_DEL,           _______,          _______, _______, _______, _______, _______  \
+    _______, _PUSHTT, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______, \
+    _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______  \
   ),
   [_MEH] = LAYOUT_t33chong(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
