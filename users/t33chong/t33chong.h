8daa3635b81468dd926ff45c77e99d786acc2ff3
Tristan Chong <tristanchong@gmail.com>
2021-06-12 01:28:02 -0700

    Add ; to end of line and send enter if meh held, : if meh + shift held

--- users/t33chong/t33chong.h
+++ users/t33chong/t33chong.h
@@ -22,6 +22,7 @@ enum _keycodes {
   _FMREQL,              // Former = key: send = if modifier held, else brightness up
   _FMRBSP,              // Former backspace key: send backspace if modifier held, else volume down
   _FMRDEL,              // Former delete key: send delete if modifier held, else volume up
+  _CLNENT,              // Add ; to end of line and send enter if meh held, : if meh + shift held
 };
 
 #define _CTLESC CTL_T(KC_ESC)              // Hold for control, tap for escape
@@ -69,6 +70,7 @@ enum _keycodes {
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_BTN5,          _______, \
     KC_LCTL, KC_LALT, KC_LGUI,          KC_BTN2,          KC_BTN3,          KC_BTN1,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 
+// TODO: change rgb underglow brightness with key
 #define _FUNCTION_MAP \
     _CLRKBD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  BL_TOGG, _RESETT, \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, \
@@ -79,6 +81,6 @@ enum _keycodes {
 #define _QUANTUM_MAP \
     KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_BSLS, \
-    KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, \
+    KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _CLNENT, _______,                   _______, \
     _ALTBSP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______, \
     _______, _______, _______,          _______,          _TO_MSK,          _______,          _______, _______, _______, _______, _______
