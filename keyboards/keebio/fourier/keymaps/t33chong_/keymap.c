1ba1474c95ff8620eb5b19292a6e43ce3353168e
Tristan Chong <tristanchong@gmail.com>
2021-08-07 03:37:50 -0700

    Clean up

--- keyboards/keebio/fourier/keymaps/t33chong_/keymap.c
+++ keyboards/keebio/fourier/keymaps/t33chong_/keymap.c
@@ -23,10 +23,7 @@
 enum _layers {
   _DEFAULT_LAYER = 0,
   _NUMERALS_LAYER,
-/* #ifdef MOUSEKEY_ENABLE */
   _MOUSEKEYS_LAYER,
-/* #endif */
-  /* _ARROWS_LAYER, */
   _FUNCTION_LAYER,
   _QUANTUM_LAYER,
 };
@@ -38,21 +35,16 @@ enum _keycodes {
   _RPAREN,              // ), or ] if shift is held
   _BAKSPC,              // Backspace, or alt+backspace if shift is held
   _UPDOWN,              // If left+right are held, then up; else down
+  _CLRKBD,              // Clear held keys
 };
 
 #define _CTLESC CTL_T(KC_ESC)                // Hold for control, tap for escape
 #define _QUASPC LT(_QUANTUM_LAYER, KC_SPC)   // Hold for quantum layer, tap for space
 #define _GNUMIN LT(_NUMERALS_LAYER, KC_MINS) // Hold for gui+number, tap for minus
-/* #define _ARRUND LT(_ARROWS_LAYER, KC_NUBS)   // Hold for arrows layer, tap for underscore */
-/* #define _ARRUND LT(_FUNCTION_LAYER, KC_NUBS)   // Hold for arrows layer, tap for underscore */
 #define _MO_FUN MO(_FUNCTION_LAYER)          // Hold for function layer
 #define _MO_NUM MO(_NUMERALS_LAYER)          // Hold for numerals layer
 #define _TO_DEF TO(_DEFAULT_LAYER)           // Activate default layer
-/* #ifdef MOUSEKEY_ENABLE */
 #define _TO_MSK TO(_MOUSEKEYS_LAYER)         // Activate mousekeys layer
-/* #else */
-/* #define _TO_MSK _______ */
-/* #endif */
 #define _PUSHTT HYPR(KC_BSLS)                // Hold for push to talk with Shush
 #define _GUIGRV G(KC_GRV)                    // Send gui+`
 #define _HF(N) HYPR(KC_F ## N)               // Send hyper+function N
@@ -64,7 +56,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, KC_EQL,
     _CTLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     _LPAREN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _RPAREN,
-    /* _MO_FUN, KC_LALT, KC_LGUI, _GNUMIN, KC_LSFT, _MO_NUM,          _BAKSPC, _QUASPC, _ARRUND, KC_LEFT, _UPDOWN, KC_RGHT */
     _MO_FUN, KC_LALT, KC_LGUI, _GNUMIN, KC_LSFT, _MO_NUM,          _BAKSPC, _QUASPC, _UNDSCR, KC_LEFT, _UPDOWN, KC_RGHT
   ),
   [_NUMERALS_LAYER] = LAYOUT_t33chong(
@@ -73,23 +64,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCBR, _HF(1),  _HF(2),  _HF(3),  _HF(4),  _HF(5),           _HF(6),  _HF(7),  _HF(8),  _HF(9),  _HF(10), KC_RCBR,
     _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______
   ),
-/* #ifdef MOUSEKEY_ENABLE */
   [_MOUSEKEYS_LAYER] = LAYOUT_t33chong(
     XXXXXXX, XXXXXXX, KC_WBAK, KC_MS_U, KC_WFWD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _TO_DEF, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,          KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, KC_BTN2, KC_BTN4,          KC_BTN3, KC_BTN1, KC_BTN5, XXXXXXX, XXXXXXX, XXXXXXX
   ),
-/* #endif */
-  /* [_ARROWS_LAYER] = LAYOUT_t33chong( */
-  /*   XXXXXXX, XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, */
-  /*   KC_LCTL, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, */
-  /*   KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, */
-  /*   XXXXXXX, KC_LALT, KC_LGUI, KC_LALT, KC_LGUI, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX */
-  /* ), */
   [_FUNCTION_LAYER] = LAYOUT_t33chong(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
+    _CLRKBD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, _PUSHTT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
@@ -107,32 +90,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* ), */
 };
 
-/* #ifdef RGBLIGHT_ENABLE */
 // Indicate layers with RGB underglow
 enum _rgblight_layer_indices {
-  _BLUE_RGBLIGHT_LAYER = 0,
-  _CYAN_RGBLIGHT_LAYER,
-  _GREEN_RGBLIGHT_LAYER,
-  _MAGENTA_RGBLIGHT_LAYER,
   _RED_RGBLIGHT_LAYER,
-  _YELLOW_RGBLIGHT_LAYER
+  _GREEN_RGBLIGHT_LAYER,
+  _YELLOW_RGBLIGHT_LAYER,
+  _CYAN_RGBLIGHT_LAYER,
+  _MAGENTA_RGBLIGHT_LAYER
 };
 
 #define _rgb_all(color) RGBLIGHT_LAYER_SEGMENTS({0, RGBLED_NUM, color})
-const rgblight_segment_t PROGMEM _blue_rgblight_layer[] = _rgb_all(HSV_BLUE);
-const rgblight_segment_t PROGMEM _cyan_rgblight_layer[] = _rgb_all(HSV_CYAN);
-const rgblight_segment_t PROGMEM _green_rgblight_layer[] = _rgb_all(HSV_GREEN);
-const rgblight_segment_t PROGMEM _magenta_rgblight_layer[] = _rgb_all(HSV_MAGENTA);
 const rgblight_segment_t PROGMEM _red_rgblight_layer[] = _rgb_all(HSV_RED);
+const rgblight_segment_t PROGMEM _green_rgblight_layer[] = _rgb_all(HSV_GREEN);
 const rgblight_segment_t PROGMEM _yellow_rgblight_layer[] = _rgb_all(HSV_YELLOW);
+const rgblight_segment_t PROGMEM _cyan_rgblight_layer[] = _rgb_all(HSV_CYAN);
+const rgblight_segment_t PROGMEM _magenta_rgblight_layer[] = _rgb_all(HSV_MAGENTA);
 
 const rgblight_segment_t* const PROGMEM _rgblight_layers[] = RGBLIGHT_LAYERS_LIST(
-  _blue_rgblight_layer,
-  _cyan_rgblight_layer,
-  _green_rgblight_layer,
-  _magenta_rgblight_layer,
   _red_rgblight_layer,
-  _yellow_rgblight_layer
+  _green_rgblight_layer,
+  _yellow_rgblight_layer,
+  _cyan_rgblight_layer,
+  _magenta_rgblight_layer
 );
 
 // Execute on startup
@@ -140,7 +119,6 @@ void keyboard_post_init_user(void) {
   rgblight_layers = _rgblight_layers;
   rgblight_enable_noeeprom();
 }
-/* #endif */
 
 // If true, don't count a tap and a hold as repetition of the tap action
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
@@ -167,7 +145,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 // Vim mode user hooks: set custom state
 void insert_mode_user(void) {
   disable_vim_mode();
-/* #ifdef RGBLIGHT_ENABLE */
   rgblight_set_layer_state(_GREEN_RGBLIGHT_LAYER, false);
   rgblight_set_layer_state(_YELLOW_RGBLIGHT_LAYER, false);
 }
@@ -183,7 +160,6 @@ void visual_mode_user(void) {
 
 void visual_line_mode_user(void) {
   rgblight_set_layer_state(_YELLOW_RGBLIGHT_LAYER, true);
-/* #endif */
 }
 
 // Vim process mode user hooks: override keycodes
@@ -239,14 +215,9 @@ int _current_layer;
 layer_state_t layer_state_set_user(layer_state_t state) {
   _current_layer = get_highest_layer(state);
 
-/* #ifdef RGBLIGHT_ENABLE */
   rgblight_set_layer_state(_RED_RGBLIGHT_LAYER, (layer_state_cmp(state, _DEFAULT_LAYER || layer_state_cmp(state, _NUMERALS_LAYER)) || layer_state_cmp(state, _QUANTUM_LAYER)));
-/* #ifdef MOUSEKEY_ENABLE */
   rgblight_set_layer_state(_CYAN_RGBLIGHT_LAYER, layer_state_cmp(state, _MOUSEKEYS_LAYER));
-/* #endif */
-  /* rgblight_set_layer_state(_BLUE_RGBLIGHT_LAYER, layer_state_cmp(state, _ARROWS_LAYER)); */
   rgblight_set_layer_state(_MAGENTA_RGBLIGHT_LAYER, layer_state_cmp(state, _FUNCTION_LAYER));
-/* #endif */
 
   return state;
 }
@@ -271,10 +242,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint16_t _pressed_updown_keycode = _NULVAL;
   static uint16_t _pressed_numerals_keycode = _NULVAL;
   static uint16_t _ctlesc_press_timer;
-
-/* #ifdef CONSOLE_ENABLE */
-/*   uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count); */
-/* #endif */
 
   switch (keycode) {
     // Set held modifier state
@@ -381,19 +348,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
 
-    /* case LSFT(KC_NUBS): */
-    /* case KC_NUBS: // Repurpose to send _, since shifted keys don't work with layer tap */
     case _UNDSCR: // Repurpose to send _, since shifted keys don't work with layer tap
       if (record->event.pressed) {
-        /* if (_is_shift_held) { */
-        /*   unregister_code(KC_LSFT); */
-        /* } */
         register_code16(S(KC_MINS));
       } else {
         unregister_code16(S(KC_MINS));
-        /* if (_is_shift_held) { */
-        /*   register_code(KC_LSFT); */
-        /* } */
       }
       return false;
 
@@ -434,6 +393,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           unregister_code16(_pressed_rparen_keycode);
           _pressed_rparen_keycode = _NULVAL;
         }
+      }
+      return false;
+
+    case _CLRKBD:
+      if (record->event.pressed) {
+        clear_keyboard();
       }
       return false;
 
--- keyboards/keebio/fourier/keymaps/t33chong_/rules.mk
+++ keyboards/keebio/fourier/keymaps/t33chong_/rules.mk
@@ -2,4 +2,3 @@ include keyboards/keebio/fourier/keymaps/$(KEYMAP)/qmk-vim/rules.mk
 BOOTLOADER = atmel-dfu
 MOUSEKEY_ENABLE = yes
 RGBLIGHT_ENABLE = yes
-# CONSOLE_ENABLE = yes
