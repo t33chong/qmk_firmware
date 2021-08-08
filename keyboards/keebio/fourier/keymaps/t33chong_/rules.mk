281e4a51702782ae71746cf76f1f9ecb34340bdc
Tristan Chong <tristanchong@gmail.com>
2021-08-08 12:45:18 -0700

    Use RGBLED_SPLIT, change Fourier keymap rules

--- keyboards/keebio/fourier/keymaps/t33chong_/rules.mk
+++ keyboards/keebio/fourier/keymaps/t33chong_/rules.mk
@@ -1,4 +1,7 @@
 include keyboards/keebio/fourier/keymaps/$(KEYMAP)/qmk-vim/rules.mk
 BOOTLOADER = atmel-dfu
+COMMAND_ENABLE = no
+EXTRAKEY_ENABLE = no
 MOUSEKEY_ENABLE = yes
+NKRO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
