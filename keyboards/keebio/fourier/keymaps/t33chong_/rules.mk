c6987a03ca4414d795cf36810ace466e557cfadf
Tristan Chong <tristanchong@gmail.com>
2021-09-18 00:41:52 -0700

    Add vol/bri controls; make default light white, hold gnumin for alt+bksp

--- keyboards/keebio/fourier/keymaps/t33chong_/rules.mk
+++ keyboards/keebio/fourier/keymaps/t33chong_/rules.mk
@@ -1,7 +1,6 @@
 include keyboards/keebio/fourier/keymaps/$(KEYMAP)/qmk-vim/rules.mk
 BOOTLOADER = atmel-dfu
 COMMAND_ENABLE = no
-EXTRAKEY_ENABLE = no
 MOUSEKEY_ENABLE = yes
 NKRO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
